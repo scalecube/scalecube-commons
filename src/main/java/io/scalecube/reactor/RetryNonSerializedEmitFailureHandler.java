8a5a94b23328e2f459157349e09dd665e61a2ce5
Artem Vysochyn <artem.vysochyn@gmail.com>
2021-08-04 22:42:39 +0300

    Added RetryNonSerializedEmitFailureHandler, added dependency on reactor

---
+++ src/main/java/io/scalecube/reactor/RetryNonSerializedEmitFailureHandler.java
@@ -0,0 +1,16 @@
+package io.scalecube.reactor;
+
+import reactor.core.publisher.SignalType;
+import reactor.core.publisher.Sinks.EmitFailureHandler;
+import reactor.core.publisher.Sinks.EmitResult;
+
+public class RetryNonSerializedEmitFailureHandler implements EmitFailureHandler {
+
+  public static final RetryNonSerializedEmitFailureHandler RETRY_NOT_SERIALIZED =
+      new RetryNonSerializedEmitFailureHandler();
+
+  @Override
+  public boolean onEmitFailure(SignalType signalType, EmitResult emitResult) {
+    return emitResult == EmitResult.FAIL_NON_SERIALIZED;
+  }
+}
